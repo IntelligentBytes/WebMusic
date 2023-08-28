f8bd33f194ecc0eb6403d351a3f5c1053c8a9a99
Sina Nabigol <sina.nabigol77@gmail.com>
2023-08-28 03:52:59 +0330

    Add music model

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from music import models
 
 # Register your models here.
+@admin.register(models.Music)
+class MusicAdmin(admin.ModelAdmin):
+    fields = ['name', 'image', 'file', 'duration', 'is_active']
+    
--- music/models.py
+++ music/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import timedelta
 
 # Create your models here.
+class Music(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    file = models.FileField(upload_to='musics/')
+    duration = models.DurationField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Music'
+        verbose_name_plural = 'Musics'
+
